75bddbccbbab4eac4264b5cef9523657e8c2334d
--unset <--unset>
2022-12-04 10:51:03 +0700

    .

--- Lect 3/main.js
+++ Lect 3/main.js
@@ -1,3 +1,62 @@
+let arr = [1, 2, "abc"]
+
+let person = {
+    name: "giang",  // index 0
+    age: 21         // index 1
+}
+
+let people = [
+    {name: "duy", age: 20},
+    {name: "khiet", age: 18},
+    {name: "khoa", age: 19}
+]
+
+let text1 = "";
+for (let x in person) {
+  text1 += " " + person[x];
+}
+
+let text2 = "";
+for (let x of arr) {
+  text2 += x + " ";
+}
+
+let text3 = "";
+for(let i = 0; i < arr.length; i++) {
+    text3 += arr[i] + " ";
+}
+
+
+let text4 = "";
+arr.forEach(x => {
+    text4 += x + " ";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
