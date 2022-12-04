fb2cbbdbc6aeaa6cedf1e2f627098ea02aaf8530
--unset <--unset>
2022-12-04 09:01:37 +0700

    .

--- Lect 3/main.js
+++ Lect 3/main.js
@@ -1,16 +1,19 @@
-function myFunction() {
-    let input = document.getElementById('mySearch');
-    let filter = input.value.toUpperCase();
-    let ul = document.getElementById("myList");
-    let li = ul.getElementsByClassName('listItem');
 
-    for (let i = 0; i < li.length; i++) {
-        let a = li[i].getElementsByClassName("itemText")[0];
-        let txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "block";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
+
+
+// function myFunction() {
+//     let input = document.getElementById('mySearch');
+//     let filter = input.value.toUpperCase();
+//     let ul = document.getElementById("myList");
+//     let li = ul.getElementsByClassName('listItem');
+
+//     for (let i = 0; i < li.length; i++) {
+//         let a = li[i].getElementsByClassName("itemText")[0];
+//         let txtValue = a.textContent || a.innerText;
+//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//             li[i].style.display = "block";
+//         } else {
+//             li[i].style.display = "none";
+//         }
+//     }
+// }
