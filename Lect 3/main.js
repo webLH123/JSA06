da3faa73c744d000106008307b7cc901e4a6e16b
--unset <--unset>
2022-12-04 11:58:40 +0700

    .

--- Lect 3/main.js
+++ Lect 3/main.js
@@ -32,47 +32,29 @@ arr.forEach(x => {
     text4 += x + " ";
 });
 
+function searchName() {
+    // khai báo DOM cho thẻ input
+    let input = document.getElementById('mySearch');
+    // chuyển dữ liệu nhập vào ở thẻ input thành dạng in hoa
+    let inputValue = input.value.toUpperCase();
+    // khai báo DOM cho thẻ ul
+    let ul = document.getElementById("myList");
+    // khai báo DOM cho tất cả thẻ li (danh sách li)
+    let li = ul.getElementsByClassName('listItem');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function myFunction() {
-//     let input = document.getElementById('mySearch');
-//     let filter = input.value.toUpperCase();
-//     let ul = document.getElementById("myList");
-//     let li = ul.getElementsByClassName('listItem');
-
-//     for (let i = 0; i < li.length; i++) {
-//         let a = li[i].getElementsByClassName("itemText")[0];
-//         let txtValue = a.textContent || a.innerText;
-//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//             li[i].style.display = "block";
-//         } else {
-//             li[i].style.display = "none";
-//         }
-//     }
-// }
+    // sử dụng vòng lặp để lặp qua hết danh sách thẻ li
+    for (let i = 0; i < li.length; i++) {
+        // khai báo DOM cho thẻ a nằm trong thẻ li
+        let a = li[i].getElementsByClassName("itemText")[0];
+        // tạo biến chứa dữ liệu text từ thẻ a
+        let txtValue = a.textContent || a.innerText;
+        // kiểm tra bằng hàm indexOf xem có lấy được chữ từ txtValue không
+        if (txtValue.toUpperCase().indexOf(inputValue) > -1) {
+            // nếu được thì hiển thị thẻ lên
+            li[i].style.display = "block";
+        } else {
+            // nếu không thì ẩn thẻ đi
+            li[i].style.display = "none";
+        }
+    }
+}
